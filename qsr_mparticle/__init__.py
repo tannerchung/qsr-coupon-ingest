f842d9abee3a0a1ac3e0539d466aecf3ca9a8224
Tanner Chung <tannerchung@users.noreply.github.com>
2025-05-21 18:23:09 -0400

    Create __init__.py

---
+++ qsr_mparticle/__init__.py
@@ -0,0 +1,8 @@
+"""
+QSR Coupon Integration for mParticle
+
+A Python-based solution for importing coupon data from CSV files 
+into mParticle for a Quick Service Restaurant (QSR) customer.
+"""
+
+__version__ = '1.0.0'
